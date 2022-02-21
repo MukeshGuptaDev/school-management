00b792729227e0222ec235ba0ae7d7040d75d1d0
Mukesh Kumar Gupta <mg98696@gmail.com>
2022-02-21 18:11:49 +0530

    Adding Date class

---
+++ src/utility/date.hpp
@@ -0,0 +1,28 @@
+#ifndef SRC_UTILITY_H
+#define SRC_UTILITY_H
+
+#include <string>
+#include<array>
+
+class Date{
+  private:
+    int date_, month_, year_;
+    const std::array<std::string, 12> monthInWord = {"January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November","December"};
+
+  public:
+    Date(int date, int month, int year);
+    void setDate(int date);
+    void setMonth(int month);
+    void setYear(int year);
+    int getDate();
+    int getMonth();
+    std::string getMonthAsString();
+    int getYear();
+  
+  private:
+    bool validateDate(int date);
+    bool validateMonth(int month);
+    bool validateYear(int year);
+};
+
+#endif
